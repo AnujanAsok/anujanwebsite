f489e02764e336dc9dbf8123afd8457ff06b07d9
AnujanAsok <anujan.asokkumar@gmail.com>
2023-04-07 20:05:16 -0400

    fixing router version

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./HomePage";
 import Experience from "./Experience";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/experiences">
-            <Experience />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/experiences" element={<Experience />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

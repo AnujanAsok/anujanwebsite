e3f1ad18609614e515154d12f6ece1477e6db006
AnujanAsok <55774590+AnujanAsok@users.noreply.github.com>
2024-05-14 22:51:07 -0400

    Update Experience.js

--- src/Experience.js
+++ src/Experience.js
@@ -5,14 +5,14 @@ const Experience = () => {
   const experience = [
     {
       company: "Afterwork",
-      position: "Software Engineering",
+      position: "Software Engineer",
       duration: "May 2024 - Present",
       description:
         "Afterwork is a corporate event planning platform, where you can service all your event needs in one place! My role at Afterwork is to build out the AI tech stack and ensure that clients have the tools they need to set up their events quicker and easier.",
     },
     {
       company: "Accent",
-      position: "Software Engineering",
+      position: "Software Engineer",
       duration: "Jan 2022 - Jan 2023",
       description:
         "Accent is a B2B gifting platform with a focus on making it easier to find and send gifts for a broad range of diverse holidays and occasions. My main focus is on developing full stack tech to provide tools for users to experience easier gifting.",

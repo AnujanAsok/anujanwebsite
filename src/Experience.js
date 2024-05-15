3d637998c54884003d3b704358ba2c09737a982b
AnujanAsok <55774590+AnujanAsok@users.noreply.github.com>
2024-05-14 22:40:32 -0400

    Update Experience.js

--- src/Experience.js
+++ src/Experience.js
@@ -5,17 +5,17 @@ const Experience = () => {
   const experience = [
     {
       company: "Afterwork",
-      position: "Chief Technology Officer",
-      duration: "Jan 2023 - Present",
+      position: "Software Engineering",
+      duration: "May 2024 - Present",
       description:
-        "Afterwork is a corporate event planning platform, where you can service all your event needs in one place! My role at Afterwork is to build out our tech stack and ensure our clients have the tools they need to set up their events quicker and easier.",
+        "Afterwork is a corporate event planning platform, where you can service all your event needs in one place! My role at Afterwork is to build out the AI tech stack and ensure that clients have the tools they need to set up their events quicker and easier.",
     },
     {
       company: "Accent",
-      position: "Chief Technology Officer",
+      position: "Software Engineering",
       duration: "Jan 2022 - Jan 2023",
       description:
-        "Accent is a B2B gifting platform with a focus on making it easier to find and send gifts for a broad range of diverse holidays and occasions. My main focus is on developing our tech stack to provide tools for clients to experience easier gifting.",
+        "Accent is a B2B gifting platform with a focus on making it easier to find and send gifts for a broad range of diverse holidays and occasions. My main focus is on developing full stack tech to provide tools for users to experience easier gifting.",
     },
   ];
   return (
